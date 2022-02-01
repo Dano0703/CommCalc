d23631d5e507d3224bfa9a5837aa55720d08aabf
4400111210 <4400111210@BT144-PROG13>
2022-02-01 14:07:27 -0700

    Add project files.

---
+++ CommCalc/CommBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommCalc
+
+{
+    class BaseCommission
+    {
+        private const decimal ITEM1 = 239.99M;
+        private const decimal ITEM2 = 126.75M;
+        private const decimal ITEM3 = 99.95M;
+        private const decimal ITEM4 = 350.89M;
+        private const decimal PAY_PER_WEEK = 200;
+        private const decimal PERCENTAGE = 0.09M;
+
+        public BaseCommission(int item1Sold, int item2Sold, int item3Sold, int item4Sold)
+        {
+            Item1Sold = item1Sold;
+            Item2Sold = item2Sold;
+            Item3Sold = item3Sold;
+            Item4Sold = item4Sold;
+        }
+
+        public int Item1Sold { get; set; }
+        public int Item2Sold { get; set; }
+        public int Item3Sold { get; set; }
+        public int Item4Sold { get; set; }
+
+        public decimal GetTotalSales()
+        {
+            return ITEM1 * Item1Sold + ITEM2 * Item2Sold + ITEM3 * Item3Sold + ITEM4 * Item4Sold;
+        }
+
+        public decimal Commission()
+        {
+            return GetTotalSales() * PERCENTAGE + PAY_PER_WEEK;
+        }
+    }
+}
---
+++ CommCalc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CommCalc
+
+{
+    public class CommissionCalc
+    {
+        static void Main()
+        {
+            int item1Sold;
+            int item2Sold;
+            int item3Sold;
+            int item4Sold;
+
+
+            Console.WriteLine("Number of Item 1 Sold:");
+            item1Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number of Item 2 Sold:");
+            item2Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number of Item 3 Sold:");
+            item3Sold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number of Item 4 Sold:");
+            item4Sold = Convert.ToInt32(Console.ReadLine());
+
+            BaseCommission cs = new(item1Sold, item2Sold, item3Sold, item4Sold);
+
+            Console.WriteLine("Total Earnings - Plus base pay - equals: {0:C}", cs.Commission());
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
